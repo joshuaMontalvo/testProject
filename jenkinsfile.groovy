72c8041ffa1a69a367390628c117211bad242ba2
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-06-27 15:51:59 -0400

    Last one I swear to God

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,10 +4,14 @@ node {
 		
 echo 'Building...'
 
-		echo 'I am a donkey'
+		bat 'cd C:\Users\joshua.montalvo\OneDrive - Accenture\Documents\testProject'
+		bat 'javac HelloWorld.java'
 	}
 	stage('Deploy')
  {  
-		echo 'Such a stupid donkey'	
+		echo 'Deploying...'
+		bat 'java HelloWorld'
+		echo 'I'll be so happy once I see this...'
+	
 	}
 }
