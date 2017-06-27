a98fcbe056ccee39dd9506a20468caca1f008c62
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-06-27 15:47:20 -0400

    I was wrong

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,13 +1,15 @@
 node {
    
-	stage 'Build'
-	
+	stage('Build') {
+		
 echo 'Building...'
 
-	bat 'cd C:\Users\joshua.montalvo\OneDrive - Accenture\Documents\testProject'
-	bat 'javac HelloWorld.java'
-	stage 'Deploy'
-   
-	bat 'java HelloWorld'
+		bat 'cd C:\Users\joshua.montalvo\OneDrive - Accenture\Documents\testProject'
+		bat 'javac HelloWorld.java'
+	}
+	stage('Deploy')
+ {  
+		bat 'java HelloWorld'
 
+	}
 }
