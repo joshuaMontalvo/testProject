3adfbedaad9404d8176e3f8c8953bb4fcb27db18
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-06-27 15:30:17 -0400

    Stop reading these messages

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,25 +1,27 @@
-node {
+pipeline {
+	agent any
+		node {
    
-	stage 'Build'
-	
+			stage 'Build'
+			
 echo 'Building...'
 
-	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '5f30d748-1b60-4c81-bf04-3bab224975ba', url: 'https://github.com/joshuaMontalvo/testProject.git']]])
-	bat 'javac HelloWorld.java'
-	archiveArtifacts artifacts: 'C:\Program Files (x86)\Jenkins\workspace\${jar.dir}\HelloWorld.jar, fingerprint: true
+			bat 'javac HelloWorld.java'
+			archiveArtifacts artifacts: 'C:\Program Files (x86)\Jenkins\workspace\${jar.dir}\HelloWorld.jar, fingerprint: true
    
-	stage 'Deploy'
+			stage 'Deploy'
    
-		when {
+			when {
        
-			expession {
+					expession {
            
-				currentBuild.result == null || currentBuild.result == 'SUCCESS'
+						currentBuild.result == null || currentBuild.result == 'SUCCESS'
        
+						}
+   
 				}
    
-		}
-   
-	bat 'java HelloWorld'
+			bat 'java HelloWorld'
 
+		}
 }
