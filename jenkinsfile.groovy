d7d206f0d7be7176f0f8e5f488a996761618abeb
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-06-27 15:25:16 -0400

    Third try

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -4,8 +4,9 @@ node {
 	
 echo 'Building...'
 
+	checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: '5f30d748-1b60-4c81-bf04-3bab224975ba', url: 'https://github.com/joshuaMontalvo/testProject.git']]])
 	bat 'javac HelloWorld.java'
-	archiveArtifacts artifacts: '**/target/*.jar', fingerprint: true
+	archiveArtifacts artifacts: 'C:\Program Files (x86)\Jenkins\workspace\${jar.dir}\HelloWorld.jar, fingerprint: true
    
 	stage 'Deploy'
    
