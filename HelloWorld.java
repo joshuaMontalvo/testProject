57e72d5e04a606a22d5bf04ef8dd2dc31cd64501
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-06-28 10:27:08 -0400

    testing trigger

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,7 @@
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("First Pipeline!");
+        System.out.print("First Pipeline!!! And in only 60 builds.");
     }
 
 }
