29ee79d8dda0ed91ad7d75ae78ec0d33425ee76c
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-07-06 11:31:57 -0400

    Trying out vars

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,17 +1,21 @@
 #!/usr/bin/env groovy
 
+def vars = [projectName:'Test Pipeline',
+	projectType:'JDK']
+
 node {
 
 	stage('Build') {
 
 		echo 'Building...'
+		echo "Using ${projectType}"
 		bat 'javac C:/testProject/HelloWorld.java'
 
 	}
 
 	stage('Deploy') {
 
-		echo 'Deploying...'
+		echo "Deploying ${projectName}..."
 		bat 'java -classpath C:/testProject HelloWorld'
 
 	}
