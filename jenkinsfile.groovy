71b4278d66dc2428470021293e7991a8e45d8ab7
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-07-10 11:05:03 -0400

    3

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,29 +3,29 @@
 pipeline {
 
 	agent any
-
-	environment {
 		
-		vars = [projectName:'Test Pipeline',
-			projectType:'JDK']
-	
-	}
+	def vars = [projectName:'Test Pipeline',
+		projectType:'JDK']	
 
 	stages {
 
 		stage('Build') {
+			
 			steps {
 				echo 'Building...'
 				echo "Using ${vars.projectType}"
 				bat 'javac C:/testProject/HelloWorld.java'
 			}
+
 		}
 
 		stage('Deploy') {
+			
 			steps {
 				echo "Deploying ${vars.projectName}..."
 				bat 'java -classpath C:/testProject HelloWorld'
 			}
+
 		}
 	
 	}
