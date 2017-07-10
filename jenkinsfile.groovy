7d97f599d0dcb5a15dbec590305115393e8fd968
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-07-10 11:20:28 -0400

    Reverting back to scripted

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,33 +1,23 @@
 #!/usr/bin/env groovy
 
-pipeline {
+node {
 
-	agent any
-		
 	def vars = [projectName:'Test Pipeline',
 		projectType:'JDK']	
 
-	stages {
-
-		stage('Build') {
-			
-			steps {
-				echo 'Building...'
-				echo "Using ${vars.projectType}"
-				bat 'javac C:/testProject/HelloWorld.java'
-			}
-
-		}
-
-		stage('Deploy') {
-			
-			steps {
-				echo "Deploying ${vars.projectName}..."
-				bat 'java -classpath C:/testProject HelloWorld'
-			}
-
-		}
-	
-	}
+ 	stage('Build') {
+  
+ 
+ 		echo "Using ${vars.projectType}"
+  		bat 'javac C:/testProject/HelloWorld.java'
+  
+  	}
+  
+  	stage('Deploy') {
+  
+ 		echo "Deploying ${vars.projectName}..."
+  		bat 'java -classpath C:/testProject HelloWorld'
+  
+  	}
 
 }
