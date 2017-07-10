d304073fe46419b291bad1029141775af5cd4463
joshuaMontalvo <joshua.montalvo2@upr.edu>
2017-07-10 11:02:03 -0400

    Trying declarative

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,23 +1,33 @@
 #!/usr/bin/env groovy
 
-def vars = [projectName:'Test Pipeline',
-	projectType:'JDK']
+pipeline {
 
-node {
+	agent any
 
-	stage('Build') {
-
-		echo 'Building...'
-		echo "Using ${vars.projectType}"
-		bat 'javac C:/testProject/HelloWorld.java'
-
+	environment {
+		
+		def vars = [projectName:'Test Pipeline',
+			projectType:'JDK']
+	
 	}
 
-	stage('Deploy') {
+	stages {
 
-		echo "Deploying ${vars.projectName}..."
-		bat 'java -classpath C:/testProject HelloWorld'
+		stage('Build') {
 
+			echo 'Building...'
+			echo "Using ${vars.projectType}"
+			bat 'javac C:/testProject/HelloWorld.java'
+
+		}
+
+		stage('Deploy') {
+
+			echo "Deploying ${vars.projectName}..."
+			bat 'java -classpath C:/testProject HelloWorld'
+
+		}
+	
 	}
 
 }
